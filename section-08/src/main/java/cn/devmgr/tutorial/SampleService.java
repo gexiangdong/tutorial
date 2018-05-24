7aec0e8b9a6c8f5087a52bfcd74b84a02a869aff
gexiangdong <gexiangdong@users.noreply.github.com>
2018-05-24 21:53:22 +0800

    Update SampleService.java

--- section-08/src/main/java/cn/devmgr/tutorial/SampleService.java
+++ section-08/src/main/java/cn/devmgr/tutorial/SampleService.java
@@ -11,14 +11,21 @@ public class SampleService {
         // 方法
     }
 
+    /**
+     * 写在私有方法上的@Async注解是不起作用的，而且不会收到任何错误信息
+     */
     @Async
     private void doB() {
     }
     
     public void doC() {
+        // 在这个方法内部调用写了@Async注解的方法doA，是不会异步执行的。只能在外部调用时异步执行
         doA();
     }
     
+    /**
+     * 外部调用这个方法时，不会得到返回值，@Async注解的方法异步执行的，如果需要返回值，要用Feature<Integer>这种类型来代替
+     */
     @Async
     public int doD() {
         return 5;
