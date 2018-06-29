7125eb4857e7988e96ab1ef60507d339872f2840
gexiangdong <gexiangdong@gmail.com>
2018-06-29 17:56:19 +0800

    update

--- drafts/mvcsecurity/src/main/java/cn/devmgr/tutorial/mvc/MvcConfig.java
+++ drafts/mvcsecurity/src/main/java/cn/devmgr/tutorial/mvc/MvcConfig.java
@@ -4,11 +4,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
+@EnableWebMvc
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
     private final static Logger logger = LoggerFactory.getLogger(MvcConfig.class);
@@ -19,8 +21,6 @@ public class MvcConfig implements WebMvcConfigurer {
         logger.trace("MvcConfig.addViewControllers()");
         registry.addViewController("/").setViewName("index");
         registry.addViewController("/login").setViewName("login");
-        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
-
     }
 
 }
--- drafts/mvcsecurity/src/main/java/cn/devmgr/tutorial/mvc/WebSecurityConfig.java
+++ drafts/mvcsecurity/src/main/java/cn/devmgr/tutorial/mvc/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -14,6 +15,15 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     
     @Autowired UserService userService;
+    
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/").permitAll().antMatchers("/welcome").hasAnyRole("USER", "ADMIN")
+                .antMatchers("/getEmployees").hasAnyRole("USER", "ADMIN").antMatchers("/addNewEmployee")
+                .hasAnyRole("ADMIN").anyRequest().authenticated()
+                .and().formLogin().loginPage("/login").permitAll()
+                .and().logout().permitAll();
+    }
     
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
