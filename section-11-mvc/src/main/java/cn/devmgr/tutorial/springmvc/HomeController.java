4ae8512de3ee44cfc95acf9323441077491a7930
gexiangdong <gexiangdong@gmail.com>
2019-02-17 16:54:57 +0800

    update

--- section-11-mvc/src/main/java/cn/devmgr/tutorial/springmvc/HomeController.java
+++ section-11-mvc/src/main/java/cn/devmgr/tutorial/springmvc/HomeController.java
@@ -25,7 +25,7 @@ public class HomeController {
      * 返回HTML片段的例子，可和前台JS协作，实现动态更新页面部分内容。
      * 此例中 index.html中有一段JS，每隔1秒更新以下id为clockcontainer的div内容为下面这个方法返回的内容
      *
-     * return 的值 :: 前面的部分是模板名称，后面是模板内的fragment名称
+     * return 的值 :: 前面的部分是模板名称，后面是模板内的fragment名称; 也可以用返回元素的ID查找，用#ID，参见下面的方法showClock
      * @param model
      * @return
      */
@@ -35,4 +35,19 @@ public class HomeController {
         model.addAttribute("time", new Date());
         return "index :: currentTime";
     }
+
+    /**
+     * DOM Selectors； 和上面的showTime方法一样，只是返回值是DOM Selectors instread of fragments
+     *
+     *
+     * @param model
+     * @return
+     */
+    @GetMapping("/clock")
+    public String showClock(Model model){
+        logger.trace("showClock()");
+        model.addAttribute("time", new Date());
+        return "index :: #clock";
+    }
+
 }
